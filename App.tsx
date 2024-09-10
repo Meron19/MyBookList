853b9bfff3f4dc689290b732a5a5071fd4e64c24
Meron19 <meronamershaoo@gmail.com>
2024-09-09 23:49:23 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import MyBooksProvider from "./context/MyBooksProvider";
+
+const API_KEY =
+  "lalmanirhat::stepzen.net+1000::204fe77487127b04491414e08cc7ef94aae4dd7af37dd0bd9e0856ddfdbf4229";
+
+const client = new ApolloClient({
+  uri: "https://lalmanirhat.stepzen.net/api/nonexistent-camel/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <ApolloProvider client={client}>
+          <MyBooksProvider>
+            <Navigation colorScheme={colorScheme} />
+          </MyBooksProvider>
+        </ApolloProvider>
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
